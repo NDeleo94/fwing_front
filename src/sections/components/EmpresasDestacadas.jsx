26659cbd01f36651cd98feb62375a3723f9438a9
djauguust <dj.august.brito@gmail.com>
2023-04-25 12:44:01 -0300

    pagina inicial

---
+++ src/sections/components/EmpresasDestacadas.jsx
@@ -0,0 +1,46 @@
+export const EmpresasDestacadas = () => {
+  return (
+    <>
+      <div className="card border-primary mb-3">
+        <div className="card-header">
+          <h1 className="lead text-center">
+            <b>Empresas Destacadas</b>
+          </h1>
+          <div className="row mb-4">
+            <div className="col-3">
+              <img
+                src="https://marketplace.canva.com/EAEj17Y_k_k/2/0/1600w/canva-amarillo-y-negro-gamer-desgastado-imagen-de-perfil-de-twitch-41B81rUGLAg.jpg"
+                className="card-img"
+                alt="..."
+              />
+            </div>
+            <div className="col-9">
+              <div className="row">
+                <div className="col-4">Empresa:</div>
+                <div className="col-8">Empresa1</div>
+              </div>
+            </div>
+          </div>
+          <div className="row my-4">
+            <div className="col-3">
+              <img
+                src="https://marketplace.canva.com/EAEj17Y_k_k/2/0/1600w/canva-amarillo-y-negro-gamer-desgastado-imagen-de-perfil-de-twitch-41B81rUGLAg.jpg"
+                className="card-img"
+                alt="..."
+              />
+            </div>
+          </div>
+          <div className="row my-4">
+            <div className="col-3">
+              <img
+                src="https://marketplace.canva.com/EAEj17Y_k_k/2/0/1600w/canva-amarillo-y-negro-gamer-desgastado-imagen-de-perfil-de-twitch-41B81rUGLAg.jpg"
+                className="card-img-bottom"
+                alt="..."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/sections/components/QueEsFollowing.jsx
@@ -0,0 +1,20 @@
+export const QueEsFollowing = () => {
+  return (
+    <>
+      <h1 className="display-6">¿Qué es Following?</h1>
+      <p className="text-center">
+        Es una Aplicación Web sobre seguimiento de graduados, que combina dos
+        formas de hacerlo: Encuestas (forma tradicional) y Perfiles de Red
+        Social (forma indirecta). Combinando y analizando estas maneras, nos
+        permite construir una base de dato híbrida, más consistente, en donde
+        cada egresado obtiene su perfil en el sistema al graduarse, y sólo
+        deberá actualizarla cuando lo crea necesario. Esta app está destinada a
+        registrar las actividades laborales de los egresados de ingeniería en
+        computación de la facet (uso momentáneo) y, si bien, está enfocado
+        solamente en este grupo de profesionales, Following pretende llegar a
+        registrar los movimientos laborales de cada graduado de cada universidad
+        que quiera ser parte de este sistema.
+      </p>
+    </>
+  );
+};
