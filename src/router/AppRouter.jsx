3d24bb9bf48a8a6c2bf0eb1a91ff34f9ab5d05da
NDeleo94 <n.deleo94@gmail.com>
2023-06-05 23:35:50 -0300

    Se agrega context de login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -12,28 +12,35 @@ import {
   UniversidadPage,
 } from "../sections/pages";
 import { Footer, Navbar } from "../ui/components";
+import { useState } from "react";
+import { LoginContext } from "../context/LoginContext";
 
 export const AppRouter = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  const state = { isLogged, setIsLogged };
+
   return (
     <>
-      <Navbar />
+      <LoginContext.Provider value={state}>
+        <Navbar />
 
-      <div className="container">
-        <Routes>
-          <Route path="home" element={<InicioPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="carrera" element={<CarreraPage />} />
-          <Route path="facultad" element={<FacultadPage />} />
-          <Route path="universidad" element={<UniversidadPage />} />
-          <Route path="mmundial" element={<MMundialPage />} />
-          <Route path="mpais" element={<MPaisPage />} />
-          <Route path="mlocal" element={<MLocalPage />} />
-          <Route path="honores" element={<HonoresPage />} />
-          <Route path="/*" element={<EgresadosRoutes />} />
-        </Routes>
-      </div>
+        <div className="container">
+          <Routes>
+            <Route path="home" element={<InicioPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="carrera" element={<CarreraPage />} />
+            <Route path="facultad" element={<FacultadPage />} />
+            <Route path="universidad" element={<UniversidadPage />} />
+            <Route path="mmundial" element={<MMundialPage />} />
+            <Route path="mpais" element={<MPaisPage />} />
+            <Route path="mlocal" element={<MLocalPage />} />
+            <Route path="honores" element={<HonoresPage />} />
+            <Route path="/*" element={<EgresadosRoutes />} />
+          </Routes>
+        </div>
 
-      <Footer />
+        <Footer />
+      </LoginContext.Provider>
     </>
   );
 };
