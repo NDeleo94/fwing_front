da23762fafa2a097c8e3a2290af109d765ba582e
djauguust <dj.august.brito@gmail.com>
2023-06-09 09:35:22 -0300

    agregado useForm en login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -4,6 +4,7 @@ import { GoogleButton } from "../components/GoogleButton";
 import Modal from "react-bootstrap/Modal";
 import { useContext, useState } from "react";
 import { LoginContext } from "../../context/LoginContext";
+import { useForm } from "../../egresados/hooks/useForm";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
@@ -21,41 +22,58 @@ export const LoginPage = () => {
     });
   };
 
+  const initialForm = {
+    email: "",
+    password: "",
+  };
+
+  const { formState, onInputChange } = useForm(initialForm);
+
   return (
     <>
       <div className="container mt-5">
         <h1>LoginPage</h1>
         <hr />
         <Button variant="success" onClick={handleShow}>
-        Ingresar con e-mail
+          Ingresar con e-mail
         </Button>
-        
+
         <hr />
         <GoogleButton />
       </div>
 
       <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Ingresar</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group className="mb-3" controlId="formFacultad">
-                <Form.Label>E-mail</Form.Label>
-                <Form.Control type="email" value="" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formUniversidad">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" value="" />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="success" onClick={onLogin}>
-              Ingresar
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <Modal.Header closeButton>
+          <Modal.Title>Ingresar</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formFacultad">
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control
+                type="email"
+                value={formState.email}
+                name="email"
+                onChange={onInputChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formUniversidad">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                value={formState.password}
+                name="password"
+                onChange={onInputChange}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={onLogin}>
+            Ingresar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
