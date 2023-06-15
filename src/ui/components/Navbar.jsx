53253f42cef061fd43c79a68ee85a9754c46c63a
djauguust <dj.august.brito@gmail.com>
2023-06-15 19:01:53 -0300

    fixed boton hamburguesa de navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -5,160 +5,191 @@ import { useContext } from "react";
 import { LoginContext } from "../../context/LoginContext";
 
 export const Navbar = () => {
-  const { user, setUser, setToken } = useContext(LoginContext);
+    const { user, setUser, setToken } = useContext(LoginContext);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const onLogout = () => {
-    setUser(null);
-    setToken(null);
-    navigate("/login", {
-      replace: true,
-    });
-  };
+    const onLogout = () => {
+        setUser(null);
+        setToken(null);
+        navigate("/login", {
+            replace: true,
+        });
+    };
 
-  const onLogin = () => {
-    navigate("/login", {
-      replace: true,
-    });
-  };
+    const onLogin = () => {
+        navigate("/login", {
+            replace: true,
+        });
+    };
 
-  const toMMundial = () => {
-    navigate("/mmundial");
-  };
+    const toMMundial = () => {
+        navigate("/mmundial");
+    };
 
-  const toMPais = () => {
-    navigate("/mpais");
-  };
+    const toMPais = () => {
+        navigate("/mpais");
+    };
 
-  const toMLocal = () => {
-    navigate("/mlocal");
-  };
+    const toMLocal = () => {
+        navigate("/mlocal");
+    };
 
-  const toPerfil = () => {
-    navigate(`/perfil/${user.id}`);
-  };
+    const toPerfil = () => {
+        navigate(`/perfil/${user.id}`);
+    };
 
-  const toConfiguracion = () => {
-    navigate("/configuracion");
-  };
+    const toConfiguracion = () => {
+        navigate("/configuracion");
+    };
 
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/home">
-            <img
-              src={Logo}
-              alt=""
-              width="30"
-              height="30"
-              className="d-inline-block align-text-top"
-            />
-            Following
-          </Link>
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo01"
+                        aria-controls="navbarTogglerDemo01"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Link className="navbar-brand" to="/home">
+                        <img
+                            src={Logo}
+                            alt=""
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-text-top"
+                        />
+                        Following
+                    </Link>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarTogglerDemo01"
+                    >
+                        <div className="navbar-collapse">
+                            <div className="navbar-nav">
+                                <NavLink
+                                    className={({ isActive }) =>
+                                        `nav-item nav-link ${
+                                            isActive ? "active" : ""
+                                        } `
+                                    }
+                                    to="/carrera"
+                                >
+                                    Carrera
+                                </NavLink>
 
-          <div className="navbar-collapse">
-            <div className="navbar-nav">
-              <NavLink
-                className={({ isActive }) =>
-                  `nav-item nav-link ${isActive ? "active" : ""} `
-                }
-                to="/carrera"
-              >
-                Carrera
-              </NavLink>
+                                <NavLink
+                                    className={({ isActive }) =>
+                                        `nav-item nav-link ${
+                                            isActive ? "active" : ""
+                                        } `
+                                    }
+                                    to="/facultad"
+                                >
+                                    Facultad
+                                </NavLink>
 
-              <NavLink
-                className={({ isActive }) =>
-                  `nav-item nav-link ${isActive ? "active" : ""} `
-                }
-                to="/facultad"
-              >
-                Facultad
-              </NavLink>
+                                <NavLink
+                                    className={({ isActive }) =>
+                                        `nav-item nav-link ${
+                                            isActive ? "active" : ""
+                                        } `
+                                    }
+                                    to="/universidad"
+                                >
+                                    Universidad
+                                </NavLink>
 
-              <NavLink
-                className={({ isActive }) =>
-                  `nav-item nav-link ${isActive ? "active" : ""} `
-                }
-                to="/universidad"
-              >
-                Universidad
-              </NavLink>
+                                <NavDropdown
+                                    title="Mercado Laboral"
+                                    id="basic-nav-dropdown"
+                                >
+                                    <NavDropdown.Item onClick={toMMundial}>
+                                        Nivel Mundial
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item onClick={toMPais}>
+                                        Nivel Pais
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item onClick={toMLocal}>
+                                        Nivel Local
+                                    </NavDropdown.Item>
+                                </NavDropdown>
 
-              <NavDropdown title="Mercado Laboral" id="basic-nav-dropdown">
-                <NavDropdown.Item onClick={toMMundial}>
-                  Nivel Mundial
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={toMPais}>
-                  Nivel Pais
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={toMLocal}>
-                  Nivel Local
-                </NavDropdown.Item>
-              </NavDropdown>
+                                <NavLink
+                                    className="nav-item nav-link"
+                                    to="/honores"
+                                >
+                                    Honores
+                                </NavLink>
+                            </div>
+                        </div>
 
-              <NavLink className="nav-item nav-link" to="/honores">
-                Honores
-              </NavLink>
-            </div>
-          </div>
-
-          <div className="row justify-content-between">
-            <div className="d-flex">
-              {/* Sin loguear */}
-              {!user ? (
-                <Button variant="success" onClick={onLogin}>
-                  Ingresar
-                </Button>
-              ) : (
-                <div className="navbar-nav">
-                  <NavDropdown
-                    title={`${user.nombres}`}
-                    id="basic-nav-dropdown"
-                  >
-                    <NavDropdown.Item onClick={toPerfil}>
-                      Perfil
-                    </NavDropdown.Item>
-                    <NavDropdown.Item onClick={toConfiguracion}>
-                      Configuración
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="">
-                      <Button variant="danger" onClick={onLogout}>
-                        Cerrar Sesión
-                      </Button>
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                        <div className="row justify-content-between">
+                            <div className="d-flex col-sm-12">
+                                {/* Sin loguear */}
+                                {!user ? (
+                                    <Button variant="success" onClick={onLogin}>
+                                        Ingresar
+                                    </Button>
+                                ) : (
+                                    <div className="navbar-nav">
+                                        <NavDropdown
+                                            title={`${user.nombres}`}
+                                            id="basic-nav-dropdown"
+                                        >
+                                            <NavDropdown.Item
+                                                onClick={toPerfil}
+                                            >
+                                                Perfil
+                                            </NavDropdown.Item>
+                                            <NavDropdown.Item
+                                                onClick={toConfiguracion}
+                                            >
+                                                Configuración
+                                            </NavDropdown.Item>
+                                            <NavDropdown.Divider />
+                                            <NavDropdown.Item href="">
+                                                <Button
+                                                    variant="danger"
+                                                    onClick={onLogout}
+                                                >
+                                                    Cerrar Sesión
+                                                </Button>
+                                            </NavDropdown.Item>
+                                        </NavDropdown>
+                                    </div>
+                                )}
+                                <div>
+                                  <br />
+                                </div>
+                                <div className="input-group ms-3 col">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Buscar"
+                                        aria-label="Buscar"
+                                        aria-describedby="button-addon2"
+                                    />
+                                    <Button
+                                        variant="btn btn-outline-secondary"
+                                        type="button"
+                                        id="button-addon2"
+                                    >
+                                        <i className="bi bi-search"></i>
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              )}
-              {/* /Sin loguear */}
-
-              {/* Logueado */}
-
-              {/* /Logueado */}
-
-              <div className="input-group ms-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Buscar"
-                  aria-label="Buscar"
-                  aria-describedby="button-addon2"
-                />
-                <Button
-                  variant="btn btn-outline-secondary"
-                  type="button"
-                  id="button-addon2"
-                >
-                  <i className="bi bi-search"></i>
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+            </nav>
+        </>
+    );
 };
