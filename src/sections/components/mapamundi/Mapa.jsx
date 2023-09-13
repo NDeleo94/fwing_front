e5798bd8eaa166caaad24f93530e4835d02dcdbb
djauguust <dj.august.brito@gmail.com>
2023-09-13 08:20:03 -0300

    agregado mapa, agregada funcion que devuelve actividades actuales

--- src/sections/components/mapamundi/Mapa.jsx
+++ src/sections/components/mapamundi/Mapa.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 export const Mapa = () => {
-  return (
-    <div>Mapa</div>
-  )
-}
+    const urlBase = import.meta.env.VITE_URL_LOCAL;
+    useEffect(() => {
+        console.log("first")
+        axios
+            .get(`${urlBase}/actividades/`)
+            .then(({ data }) => {
+                console.log(data)
+                let actActual = [];
+                data.map((a) => {
+                    if (!a.fin) {
+                        actActual = [...actActual, a];
+                    }
+                });
+                console.log(actActual);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return (
+        <>
+            <MapContainer
+                center={[30, 0]}
+                zoom={2}
+                scrollWheelZoom={false}
+                style={{ width: "100%", height: "600px" }}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </>
+    );
+};
--- src/sections/pages/FacultadPage.jsx
+++ src/sections/pages/FacultadPage.jsx
@@ -1,10 +1,17 @@
 import Foto1 from "../../assets/imgs/facet/BlockMecanicaAdentro_JPG-370x298.jpg";
 import Foto2 from "../../assets/imgs/facet/BLOCK-3-FRENTE-scaled-370x298.jpg";
 import Foto3 from "../../assets/imgs/facet/17.png";
+import { Mapa } from "../components/mapamundi/Mapa";
 
 export const FacultadPage = () => {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            <div className="text-center mt-3">Egresados por el mundo</div>
+            <div className="card mt-3">
+                <div className="card-body text-center">
+                    <Mapa />
+                </div>{" "}
+            </div>
+        </>
+    );
 };
