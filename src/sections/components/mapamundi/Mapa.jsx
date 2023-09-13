bd9f8af84b88dfff5d16824dc37c5ba90465f828
djauguust <dj.august.brito@gmail.com>
2023-09-13 13:08:45 -0300

    se avanzo con el super array que se necesita para realizar todos los marcadores

--- src/sections/components/mapamundi/Mapa.jsx
+++ src/sections/components/mapamundi/Mapa.jsx
@@ -1,25 +1,47 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { getCiudadesByActividadActual } from "../../helpers/getCiudadesByActividadActual";
 
 export const Mapa = () => {
     const urlBase = import.meta.env.VITE_URL_LOCAL;
+    const keyBingMaps = import.meta.env.VITE_KEY_BING_MAPS;
+    const urlBingMaps = import.meta.env.VITE_URL_BING_MAPS;
+    console.log(urlBingMaps);
+    const [actividadesActuales, setActividadesActuales] = useState([]);
+    const [actividades, setActividades] = useState([]);
+    const [coordenadasSimples, setCoordenadasSimples] = useState([0,0])
     useEffect(() => {
-        console.log("first")
         axios
             .get(`${urlBase}/actividades/`)
             .then(({ data }) => {
-                console.log(data)
+                setActividades(data);
                 let actActual = [];
                 data.map((a) => {
                     if (!a.fin) {
                         actActual = [...actActual, a];
                     }
                 });
-                console.log(actActual);
+                setActividadesActuales(actActual);
             })
             .catch((error) => console.log(error));
     }, []);
+    useEffect(() => {
+        axios
+            .get(`${urlBingMaps}/Tafí Viejo?o=json&key=${keyBingMaps}`)
+            .then(({ data }) => {
+                setCoordenadasSimples(data.resourceSets[0].resources[0].geocodePoints[0].coordinates);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    console.log(actividadesActuales);
+    useEffect(() => {
+        if (actividadesActuales) {
+            let arreglo = getCiudadesByActividadActual(actividadesActuales);
+            console.log(arreglo);
+        }
+    }, [actividadesActuales]);
 
     return (
         <>
@@ -36,6 +58,13 @@ export const Mapa = () => {
                 <Marker position={[51.505, -0.09]}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+                <Marker position={coordenadasSimples}>
+                    <Popup>
+                        '-26.5643582', lon: '-64.882397' A pretty CSS3 popup.{" "}
+                        -26.8303703', lon: '-65.2038133
+                        <br /> Easily customizable.
                     </Popup>
                 </Marker>
             </MapContainer>
