9ca26ea14f0655ecda725fe504c580919af5a5a9
djauguust <dj.august.brito@gmail.com>
2023-09-25 16:49:17 -0300

    listo en mapamundi que aparezcan ingenieros random en los lugares del mundo

--- src/sections/components/mapamundi/Mapa.jsx
+++ src/sections/components/mapamundi/Mapa.jsx
@@ -4,7 +4,7 @@ import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { getCiudadesByActividadActual } from "../../helpers/getCiudadesByActividadActual";
 import { getArrayForMapamundi } from "../../helpers/getArrayForMapamundi";
 import { Image } from "react-bootstrap";
-import Logo from "../../../assets/imgs/Logo.png";
+import { EgresadosEnCiudad } from "./components/EgresadosEnCiudad";
 
 export const Mapa = () => {
     const urlBase = import.meta.env.VITE_URL_LOCAL;
@@ -55,54 +55,7 @@ export const Mapa = () => {
                     <>
                         <Marker position={ciudad[1]}>
                             <Popup>
-                                <div className="text-center">
-                                    Ciudad: {ciudad[0]}
-                                    <br />
-                                    Cantidad de ingenieros: {ciudad[2]}
-                                    <br />
-                                    {ciudad[3].map((foto, index) => (
-                                        <>
-                                            {index < 3 && ( // aquí pongo el límite para que me muestre solo 3 egresados por ciudad
-                                                <>
-                                                    <Image
-                                                        src={
-                                                            ciudad[3].length < 4 // Para evitar que se repitan fotos si hay menos de 4 ingenieros en esa ciudad
-                                                                ? foto == "Logo" // Corrijo para que pueda aparecer el logo
-                                                                    ? Logo
-                                                                    : foto
-                                                                : ciudad[3][
-                                                                      Math.floor(
-                                                                          Math.random() *
-                                                                              ciudad[3]
-                                                                                  .length
-                                                                      )
-                                                                  ] == "Logo"
-                                                                ? Logo
-                                                                : ciudad[3][ // En caso de que haya más de 3 ingenieros, los elige al azar de forma independiente (pueden repetirse)
-                                                                      Math.floor(
-                                                                          Math.random() *
-                                                                              ciudad[3]
-                                                                                  .length
-                                                                      )
-                                                                  ]
-                                                        }
-                                                        style={{
-                                                            width: "45px",
-                                                            height: "auto",
-                                                        }}
-                                                        thumbnail
-                                                        className={
-                                                            index == 0 ||
-                                                            index == 2
-                                                                ? ""
-                                                                : "m-1"
-                                                        }
-                                                    />
-                                                </>
-                                            )}
-                                        </>
-                                    ))}
-                                </div>
+                                <EgresadosEnCiudad ciudad={ciudad} />
                             </Popup>
                         </Marker>
                     </>
