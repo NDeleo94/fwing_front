bbb6c3708c42ca0f544e24e81f8b7f1aa7a1f440
djauguust <dj.august.brito@gmail.com>
2023-10-05 21:27:30 -0300

    avances sobre new password

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -8,206 +8,273 @@ import { useForm } from "../../egresados/hooks/useForm";
 import { LinkedInButton } from "../components/LinkedInButton";
 
 export const LoginPage = () => {
-  const navigate = useNavigate();
-  const [show, setShow] = useState(false);
-  const { setIsLogged, setUser, setToken } = useContext(LoginContext);
-  const [waitAxios, setWaitAxios] = useState(false);
+    const navigate = useNavigate();
+    const [show, setShow] = useState(false);
+    const { setIsLogged, setUser, setToken } = useContext(LoginContext);
+    const [waitAxios, setWaitAxios] = useState(false);
 
-  const urlBase = import.meta.env.VITE_URL_LOCAL;
+    const urlBase = import.meta.env.VITE_URL_LOCAL;
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => {
-    onResetForm();
-    setShowAlert(false);
-    setValidated(false);
-    setShow(true);
-    setWaitAxios(false);
-    setForgotPassword(false);
-  };
-
-  const onLogin = (e) => {
-    handleSubmit(e);
-    setWaitAxios(true);
-    if (formState.username && formState.password) {
-      Loguearse();
-    }
-  };
-
-  function Loguearse() {
-    console.log(formState);
-    const url = `${urlBase}/login/`;
-    const postData = formState;
-
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(postData),
+    const handleClose = () => setShow(false);
+    const handleShow = () => {
+        onResetForm();
+        setShowAlert(false);
+        setValidated(false);
+        setShow(true);
+        setWaitAxios(false);
+        setForgotPassword(false);
     };
 
-    fetch(url, requestOptions)
-      .then((response) => {
-        if (response.ok) {
-          response.json().then((data) => {
-            setToken(data.token);
-            setUser(data.user);
-            setIsLogged(true);
-            setWaitAxios(false);
-            navigate("/home", {
-              replace: true,
-            });
-          });
-        } else {
-          response.json().then((data) => {
-            console.error("Error:", data.error);
-            setWaitAxios(false);
-            setShowAlert(true);
-          });
+    const onLogin = (e) => {
+        handleSubmit(e);
+        setWaitAxios(true);
+        if (formState.username && formState.password) {
+            Loguearse();
         }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        setWaitAxios(false);
-      });
-  }
+    };
 
-  const enterPulsed = (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      Loguearse();
+    function Loguearse() {
+        console.log(formState);
+        let url;
+        if (forgotPassword) {
+            url = `${urlBase}/reset-password/`;
+            const postData = { email: formState.username };
+
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(postData),
+            };
+
+            fetch(url, requestOptions)
+                .then((response) => {
+                    if (response.ok) {
+                        response.json().then((data) => {
+                            setWaitAxios(false);
+							/* Falta esta parte cuando el backend funcione */
+							console.log(data)
+                        });
+                    } else {
+                        response.json().then((data) => {
+                            console.error("Error:", data.error);
+                            setWaitAxios(false);
+                            setShowAlert(true);
+                        });
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                    setWaitAxios(false);
+                });
+        } else {
+            url = `${urlBase}/login/`;
+            const postData = formState;
+
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(postData),
+            };
+
+            fetch(url, requestOptions)
+                .then((response) => {
+                    if (response.ok) {
+                        response.json().then((data) => {
+                            setToken(data.token);
+                            setUser(data.user);
+                            setIsLogged(true);
+                            setWaitAxios(false);
+                            navigate("/home", {
+                                replace: true,
+                            });
+                        });
+                    } else {
+                        response.json().then((data) => {
+                            console.error("Error:", data.error);
+                            setWaitAxios(false);
+                            setShowAlert(true);
+                        });
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                    setWaitAxios(false);
+                });
+        }
     }
-  };
 
-  const initialForm = {
-    username: "",
-    password: "",
-  };
+    const enterPulsed = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            Loguearse();
+        }
+    };
 
-  const { formState, onInputChange, onResetForm } = useForm(initialForm);
+    const initialForm = {
+        username: "",
+        password: "",
+    };
 
-  const [showAlert, setShowAlert] = useState(false);
-  const [forgotPassword, setForgotPassword] = useState(false);
-  const [validated, setValidated] = useState(false);
+    const { formState, onInputChange, onResetForm } = useForm(initialForm);
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    const [showAlert, setShowAlert] = useState(false);
+	const [showNotification, setShowNotification] = useState(false)
+    const [forgotPassword, setForgotPassword] = useState(false);
+    const [validated, setValidated] = useState(false);
 
-    setValidated(true);
-  };
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
 
-  return (
-    <>
-      <div className="container mt-5">
-        <h1>Iniciar Sesión</h1>
-        <hr />
-        <Button variant="success" onClick={handleShow}>
-          <i className="bi bi-key-fill"></i> Iniciar sesión con contraseña
-        </Button>
+        setValidated(true);
+    };
 
-        <hr />
-        <GoogleButton />
-        <hr />
-        <LinkedInButton />
-      </div>
+    return (
+        <>
+            <div className="container mt-5">
+                <h1>Iniciar Sesión</h1>
+                <hr />
+                <Button variant="success" onClick={handleShow}>
+                    <i className="bi bi-key-fill"></i> Iniciar sesión con
+                    contraseña
+                </Button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          {forgotPassword ? (
-            <>
-              <Modal.Title>¿Olvidaste tu contraseña?</Modal.Title>
-            </>
-          ) : (
-            <Modal.Title>Ingresar a Following</Modal.Title>
-          )}
-        </Modal.Header>
-        <Modal.Body>
-          {forgotPassword ? (
-            <>
-              Ingresa tu e-mail y te enviaremos un link de recuperación.
-              <br />
-              <br />
-              <Form.Group className="mb-3" controlId="formFacultad">
-                <Form.Label>E-mail</Form.Label>
-                <Form.Control
-                  type="username"
-                  value={formState.username}
-                  name="username"
-                  onChange={onInputChange}
-                  required
-                />
-              </Form.Group>
-              <br />
-              <a
-                onClick={() => setForgotPassword(false)}
-                className="text-reset"
-              >
-                <span style={{ fontSize: "10pt" }}>
-                  <p className="text-end">Volver</p>
-                </span>
-              </a>
-            </>
-          ) : (
-            <>
-              <Form noValidate validated={validated} onSubmit={onLogin}>
-                <Form.Group className="mb-3" controlId="formFacultad">
-                  <Form.Label>E-mail o DNI</Form.Label>
-                  <Form.Control
-                    type="username"
-                    value={formState.username}
-                    name="username"
-                    onChange={onInputChange}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formUniversidad">
-                  <Form.Label>Contraseña</Form.Label>
-                  <Form.Control
-                    type="password"
-                    value={formState.password}
-                    name="password"
-                    onChange={onInputChange}
-                    onKeyDown={enterPulsed}
-                    required
-                  />
-                </Form.Group>
-              </Form>
-              {showAlert ? (
-                <Alert
-                  variant="danger"
-                  onClose={() => setShowAlert(false)}
-                  dismissible
-                >
-                  <b>¡Usuario o Contraseña incorrectas!</b>
-                </Alert>
-              ) : (
-                ""
-              )}
-              <br />
-              <a onClick={() => setForgotPassword(true)} className="text-reset">
-                <span className="mb-3 mb-md-0" style={{ fontSize: "10pt" }}>
-                  <p className="text-end">¿Olvidaste tu contraseña?</p>
-                </span>
-              </a>
-            </>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          {forgotPassword ? (
-            <Button variant="success" onClick={onLogin} disabled={waitAxios}>
-              Enviar enlace
-            </Button>
-          ) : (
-            <Button variant="success" onClick={onLogin} disabled={waitAxios}>
-              Ingresar
-            </Button>
-          )}
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+                <hr />
+                <GoogleButton />
+                <hr />
+                <LinkedInButton />
+            </div>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    {forgotPassword ? (
+                        <>
+                            <Modal.Title>¿Olvidaste tu contraseña?</Modal.Title>
+                        </>
+                    ) : (
+                        <Modal.Title>Ingresar a Following</Modal.Title>
+                    )}
+                </Modal.Header>
+                <Modal.Body>
+                    {forgotPassword ? (
+                        <>
+                            Ingresa tu e-mail y te enviaremos un link de
+                            recuperación.
+                            <br />
+                            <br />
+                            <Form.Group
+                                className="mb-3"
+                                controlId="formFacultad"
+                            >
+                                <Form.Label>E-mail</Form.Label>
+                                <Form.Control
+                                    type="username"
+                                    value={formState.username}
+                                    name="username"
+                                    onChange={onInputChange}
+                                    required
+                                />
+                            </Form.Group>
+                            <br />
+                            <a
+                                onClick={() => setForgotPassword(false)}
+                                className="text-reset"
+                            >
+                                <span style={{ fontSize: "10pt" }}>
+                                    <p className="text-end">Volver</p>
+                                </span>
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <Form
+                                noValidate
+                                validated={validated}
+                                onSubmit={onLogin}
+                            >
+                                <Form.Group
+                                    className="mb-3"
+                                    controlId="formFacultad"
+                                >
+                                    <Form.Label>E-mail o DNI</Form.Label>
+                                    <Form.Control
+                                        type="username"
+                                        value={formState.username}
+                                        name="username"
+                                        onChange={onInputChange}
+                                        required
+                                    />
+                                </Form.Group>
+                                <Form.Group
+                                    className="mb-3"
+                                    controlId="formUniversidad"
+                                >
+                                    <Form.Label>Contraseña</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        value={formState.password}
+                                        name="password"
+                                        onChange={onInputChange}
+                                        onKeyDown={enterPulsed}
+                                        required
+                                    />
+                                </Form.Group>
+                            </Form>
+                            {showAlert ? (
+                                <Alert
+                                    variant="danger"
+                                    onClose={() => setShowAlert(false)}
+                                    dismissible
+                                >
+                                    <b>¡Usuario o Contraseña incorrectas!</b>
+                                </Alert>
+                            ) : (
+                                ""
+                            )}
+                            <br />
+                            <a
+                                onClick={() => setForgotPassword(true)}
+                                className="text-reset"
+                            >
+                                <span
+                                    className="mb-3 mb-md-0"
+                                    style={{ fontSize: "10pt" }}
+                                >
+                                    <p className="text-end">
+                                        ¿Olvidaste tu contraseña?
+                                    </p>
+                                </span>
+                            </a>
+                        </>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    {forgotPassword ? (
+                        <Button
+                            variant="success"
+                            onClick={onLogin}
+                            disabled={waitAxios}
+                        >
+                            Enviar enlace
+                        </Button>
+                    ) : (
+                        <Button
+                            variant="success"
+                            onClick={onLogin}
+                            disabled={waitAxios}
+                        >
+                            Ingresar
+                        </Button>
+                    )}
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 };
--- src/sections/pages/PasswordPage.jsx
+++ src/sections/pages/PasswordPage.jsx
@@ -1,11 +1,171 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useForm } from "../../egresados/hooks/useForm";
+import { Button, Form } from "react-bootstrap";
+import { ToastNotificacionPush } from "../../egresados/components/ToastNotificacionPush";
+import axios from "axios";
 
 export const PasswordPage = () => {
-
     const { token } = useParams();
-    console.log(token)
-  return (
-    <div>PasswordPage</div>
-  )
-}
+    console.log(token);
+    const baseUrl = import.meta.env.VITE_URL_LOCAL;
+
+    const initialForm = {
+        newPassword: "",
+        repeatPassword: "",
+    };
+    const { formState, onInputChange } = useForm(initialForm);
+
+    const navigate = useNavigate();
+
+    const toLoginPage = () => {
+        navigate("/login", {
+            replace: true,
+        });
+    };
+    const [waitAxios, setWaitAxios] = useState(false);
+
+    /* Botón de "actualizar contraseña" */
+    const onSubmit = () => {
+        console.log(formState);
+        if (formState?.repeatPassword == "" || formState?.newPassword == "") {
+            CallToast("Debe llenar todos los campos", "warning");
+            return;
+        }
+        if (!(formState?.newPassword === formState?.repeatPassword)) {
+            CallToast("Las nuevas contraseñas deben ser idénticas", "warning");
+            return;
+        }
+        if (formState.newPassword.length < 8) {
+            CallToast(
+                "La nueva contraseña debe tener al menos 8 caracteres alfanuméricos",
+                "warning"
+            );
+            return;
+        }
+
+        if (!validarPassword(formState?.newPassword)) {
+            CallToast(
+                "La nueva contraseña no cumple con los requisitos mencionados",
+                "warning"
+            );
+            return;
+        }
+
+        let url = `${baseUrl}/new-password/`;
+        axios
+            .post(
+                url,
+                {
+                    new_password: formState.newPassword,
+                },
+                {
+                    headers: { Authorization: `Token ${token}` },
+                }
+            )
+            .then(({ data }) => {
+                CallToast(
+                    "¡Contraseña cambiada! Cambiando a vista de Logueo",
+                    "success"
+                );
+                setTimeout(() => {
+                    toLoginPage();
+                }, 2000);
+            })
+            .catch(({ response }) => {
+                if (response.data.error == "Invalid url") {
+                    CallToast(`token de url inexistente o vencido`, "danger");
+                } else {
+                    CallToast(`Problema: ${response.data.error}`, "danger");
+                }
+            });
+    };
+
+    const validarPassword = (p) => {
+        const longitudMinima = 8;
+        const contieneMayuscula = /[A-Z]/.test(p);
+        const contieneMinuscula = /[a-z]/.test(p);
+        const contieneNumero = /[0-9]/.test(p);
+        const contieneCaracterEspecial = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(
+            p
+        );
+
+        const esValida =
+            p.length >= longitudMinima &&
+            contieneMayuscula &&
+            contieneMinuscula &&
+            contieneNumero &&
+            contieneCaracterEspecial;
+
+        return esValida;
+    };
+    /* FIN Botón de "actualizar contraseña" */
+
+    /* TOAST */
+    /* Toast */
+    const [messageToast, setMessageToast] = useState("second");
+    const [showToast, setShowToast] = useState(false);
+    const [typeToast, setTypeToast] = useState("primary");
+    const CallToast = (message, type) => {
+        let refuerzo = (
+            <>
+                <b>{message}</b>
+            </>
+        );
+        setMessageToast(refuerzo);
+        setTypeToast(type);
+        setShowToast(true);
+        setTimeout(() => {
+            setShowToast(false);
+        }, 5100);
+    };
+    /* FIN Toast */
+    /* FIN TOAST */
+
+    return (
+        <>
+            <div className="mt-3">
+                <b>
+                    La nueva contraseña debe tener al menos 8 caracteres
+                    alfanuméricos, 1 mayúscula, 1 minúscula y 1 caracter
+                    especial
+                </b>
+            </div>
+            <Form /* onSubmit={onSubmit} */ className="my-4">
+                <Form.Group className="mb-3" controlId="formFacultad">
+                    <Form.Label>Ingrese su nueva contraseña</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={formState.newPassword}
+                        name="newPassword"
+                        onChange={onInputChange}
+                        required
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formUniversidad">
+                    <Form.Label>Repita su nueva contraseña</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={formState.repeatPassword}
+                        name="repeatPassword"
+                        onChange={onInputChange}
+                        required
+                    />
+                </Form.Group>
+                <Button
+                    variant="success"
+                    onClick={onSubmit}
+                    disabled={waitAxios}
+                >
+                    Actualizar contraseña
+                </Button>
+            </Form>
+
+            <ToastNotificacionPush
+                mensaje={messageToast}
+                mostrar={showToast}
+                tipo={typeToast}
+            />
+        </>
+    );
+};
